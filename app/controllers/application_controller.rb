431f4b5bdaa31ae56c7101aa7ac0fe99998c68ca
Manuela Solís <manuelaymanuela@hotmail.com>
2018-04-03 10:51:24 +0200

    completar modelo y controlador aplicación

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+#Método que devuelve el current_user o nil
+helper_method :current_user
+ 
+  private
+ 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+    #Este método crea un nuevo usuario en la tabla con la respuesta de omniauth
+    def self.create_with_omniauth(auth)
+      create! do |user|
+        user.provider = auth["provider"]
+        user.uid = auth["uid"]
+        user.name = auth["info"]["name"]
+        user.image = auth["info"]["image"]
+        end
+    end
 end
