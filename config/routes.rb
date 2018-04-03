895bd56b67a70eee311cbad876387a9be839abbd
Manuela Solís <manuelaymanuela@hotmail.com>
2018-04-03 11:15:10 +0200

    creando rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
+# get 'home/index'
+# get 'sessions/create'
+# get 'sessions/destroy'
 
-  get 'sessions/create'
+  root "home#index"
+  get "/auth/:provider/callback" => "sessions#create"
+  get "/signout" => "sessions#destroy", :as => :signout
 
-  get 'sessions/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
